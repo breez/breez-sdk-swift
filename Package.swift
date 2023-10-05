26ecb70efe3f646aaacfee2d6ee56c48ef522414
runner <runner@Mac-1696481935191.local>
2023-10-05 06:35:26 +0000

    Update Breez SDK Swift bindings to version 0.2.5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.2.4/breez_sdkFFI.xcframework.zip", checksum: "7e7ca42814d663f55c7ad85579b3f5496654086d27b631190ab2f42c3484f29b"),
+        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.2.5/breez_sdkFFI.xcframework.zip", checksum: "ce5ff9c21d6cb74c35f6d93110727d0127392f39f8f9c57c38fe4bb3502a8fd8"),
         .target(name: "BreezSDK", dependencies: ["breez_sdkFFI"]),
     ]
 )
