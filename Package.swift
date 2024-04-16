9c603e8b5bf4c458e6bca0c0d819cebdc62b4ec7
runner <runner@Mac-1713293057294.local>
2024-04-16 20:29:26 +0000

    Update Breez SDK Swift bindings to version 0.4.0-rc3

--- Package.swift
+++ Package.swift
@@ -10,10 +10,11 @@ let package = Package(
         .iOS(.v11),
     ],
     products: [
-        .library(name: "BreezSDK", targets: ["breez_sdkFFI", "BreezSDK"]),
+        .library(name: "BreezSDK", targets: ["breez_sdkFFI", "BreezSDK", "BreezSDKNotification"]),
     ],
     targets: [
-        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.4.0-rc2/breez_sdkFFI.xcframework.zip", checksum: "96ea2f50dd22ee879cd8af536047a0f0f8a6201683e2b5ce7ad2dfda1ad7149f"),
-        .target(name: "BreezSDK", dependencies: ["breez_sdkFFI"], sources: ["../BreezSDK", "../BreezSDKNotification"]),
+        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.4.0-rc3/breez_sdkFFI.xcframework.zip", checksum: "a1a2d0baf6b4a882c41858cf348cfa818aa400a5cee386610c1e060d156e1b41"),
+        .target(name: "BreezSDK", dependencies: ["breez_sdkFFI"]),
+        .target(name: "BreezSDKNotification", dependencies: ["BreezSDK"]),
     ]
 )
