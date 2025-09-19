8b1803b4cee4b4f6b55ad43b5d5306b35f140c1b
Anka <runner@sat12-dp146-dcf241ae-9a65-4e08-833f-cd5440ec9d82-DE1D19CFCB87.local>
2025-09-19 15:14:13 +0000

    Update Breez SDK Swift bindings to version 0.8.3-dev2

--- BreezSDK.podspec
+++ BreezSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name                   = "BreezSDK"
-  spec.version                = "0.8.3-dev1"
+  spec.version                = "0.8.3-dev2"
   spec.license                = { :type => "MIT" }
   spec.summary                = "Swift bindings to the Breez SDK"
   spec.homepage               = "https://breez.technology"
--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "BreezSDK", targets: ["breez_sdkFFI", "BreezSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.8.3-dev1/breez_sdkFFI.xcframework.zip", checksum: "97bf9a9ae4581fba18ac42d908aeb0d13497eb9af809c9fcceaaf6019a8d7fff"),
+        .binaryTarget(name: "breez_sdkFFI", url: "https://github.com/breez/breez-sdk-swift/releases/download/0.8.3-dev2/breez_sdkFFI.xcframework.zip", checksum: "5f5564853a3aa8be620760da7e6a503d366727d7557fc2ce5e3986bdf72e1594"),
         .target(name: "BreezSDK", dependencies: ["breez_sdkFFI"]),
     ]
 )
